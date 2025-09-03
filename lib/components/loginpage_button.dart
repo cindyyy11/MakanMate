b367d80325b1240b365eff5c5ee5a4246dffb31f
Charlotte-720 <charlottegoh0720@gmail.com>
2025-09-03 09:15:57 +0800

    authentication

---
+++ lib/components/loginpage_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginpageButton extends StatelessWidget {
+
+  final Function()? onTap;
+
+  const LoginpageButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.blue[700],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Center(
+          child: Text(
+            'Log In',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        )
+      ),
+    );
+  }
+}
